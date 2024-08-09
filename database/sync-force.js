d2c48cd52775fced66ca288e1e8a9e81f9292f0c
wanderson456 <franciscowanderson26@gmail.com>
2024-08-09 11:14:02 -0300

    criando tabela categoria

--- database/sync-force.js
+++ database/sync-force.js
@@ -1,4 +1,5 @@
 const connection = require('../config/connection');
 require ('../models/UserModel')
+require ('../models/CategoryModel')
 
 connection.sync({force: true});
---
+++ models/CategoryModel.js
@@ -0,0 +1,41 @@
+const {  DataTypes, Model } = require('sequelize');
+const connection = require('../config/connection');
+
+class CategoryModel extends Model {
+  
+    
+  
+}
+
+CategoryModel.init(
+  {
+    id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true },
+    name:{
+        type:DataTypes.STRING(255),
+         allowNull: false,
+    },
+    slug:{
+        type:DataTypes.STRING(255),
+         allowNull: false,
+    },
+    use_in_menu:{
+        type:DataTypes.BOOLEAN,
+        defaultValue:0,
+        allowNull: false,
+
+    },
+    
+  },
+  {
+    timestamps:true,
+   tableName: 'category',
+    sequelize: connection,
+    
+    
+  },
+);
+
+module.exports = CategoryModel;
